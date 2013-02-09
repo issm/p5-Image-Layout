37a777582608a3df4f10b9603dca290e56cf4061
issm <issmxx@gmail.com>
2013-02-09 19:10:02 +0900

    initial commit.

---
+++ lib/Image/Layout.pm
@@ -0,0 +1,111 @@
+package Image::Layout;
+
+use 5.008;
+use strict;
+use warnings FATAL => 'all';
+
+=head1 NAME
+
+Image::Layout - The great new Image::Layout!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Image::Layout;
+
+    my $foo = Image::Layout->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+IWATA, Susumu, C<< <issmxx at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-image-layout at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Image-Layout>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Image::Layout
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Image-Layout>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Image-Layout>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Image-Layout>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Image-Layout/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2013 IWATA, Susumu.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See L<http://dev.perl.org/licenses/> for more information.
+
+
+=cut
+
+1; # End of Image::Layout
